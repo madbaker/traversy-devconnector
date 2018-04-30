a6c10ddf2f42591c4113c963dd355eee2e371b89
Mark Dyck <mark@madbaker.com>
2018-04-30 12:50:02 -0600

    establish initial route files

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Bring in Routes
+
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
+//Init App
+
 const app = express();
 
 //DB Config
@@ -14,8 +22,18 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// @route   GET /
+// @desc    Placeholder route for now
+// @access  Public
 app.get("/", (req, res) => res.send("Hello World from DevConnector"));
 
+//Use Routes
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+//Run the server
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
