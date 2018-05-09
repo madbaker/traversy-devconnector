6004e32836e48223dc9f81c4154f40687a8e004d
Mark Dyck <mark@madbaker.com>
2018-05-09 13:34:42 -0600

    add routes to add and delete likes and comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -92,4 +92,143 @@ router.delete(
   }
 );
 
+// @route   POST api/posts/like:id
+// @desc    Add a like to a post
+// @access  Private
+
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+          //User has already liked it
+          return res
+            .status(400)
+            .json({ alreadyliked: "User already liked this post" });
+        }
+        //Add user id to likes array
+        post.likes.unshift({ user: req.user.id });
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: "No post found with that id" })
+      );
+  }
+);
+
+// @route   POST api/posts/unlike:id
+// @desc    Remove a like to a post
+// @access  Private
+
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+          //User has not  liked it
+          return res
+            .status(400)
+            .json({ notliked: "User has not liked this post" });
+        }
+        //Get remove index
+        const removeIndex = post.likes
+          .map(item => item.user.toString())
+          .indexOf(req.user.id);
+
+        //Splice it out of the array
+
+        post.likes.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: "No post found with that id" })
+      );
+  }
+);
+
+// @route   POST api/posts/comment/:id
+// @desc    Add a comment to a post
+// @access  Private
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation - same as PostInput
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+        //Add to the comments array
+        post.comments.unshift(newComment);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: "No post found with that id" })
+      );
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:commentid
+// @desc    Delete a comment from a post
+// @access  Private
+
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          //User has not  liked it
+          return res.status(400).json({ nocomment: "Comment does not exist" });
+        }
+        //Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.param.comment_id);
+
+        //Make sure only the comment owner can delete comment
+        if (req.user.id !== post.comments[removeIndex].user.toString()) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+
+        //Splice it out of the array
+
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err =>
+        res.status(404).json({ nopostfound: "No post found with that id" })
+      );
+  }
+);
 module.exports = router;
